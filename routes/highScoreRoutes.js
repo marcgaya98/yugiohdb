bb10170bad6ab12dc7542995445b5e837c4f3a75
Marc Gaya <marcgaya98@gmail.com>
2025-07-02 11:58:27 +0200

    refactor: Renombrar modelo HighScoreType a HighScore para mejor consistencia

--- routes/highScoreRoutes.js
+++ routes/highScoreRoutes.js
@@ -5,7 +5,7 @@ import { validateIdParam } from '../middleware/validateParams.js';
 const router = Router();
 
 router.get('/', HighScoreController.getAllHighScores);
-router.get('/:id', validateIdParam, HighScoreController.getHighScoreById);
 router.get('/category/:category', HighScoreController.getHighScoresByCategory);
+router.get('/:id', validateIdParam, HighScoreController.getHighScoreById);
 
 export default router;
